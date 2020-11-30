a01f457d577b353b207ee23310a661995b93bf8f
fernancumez <fernancumez@gmail.com>
2020-11-30 11:16:37 -0600

    routes initial config

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,7 @@
 import { Router } from "express";
+import { helloworld } from "../controllers/photo.controller";
 const router = Router();
+
+router.route("/").get(helloworld);
 
 export default router;
