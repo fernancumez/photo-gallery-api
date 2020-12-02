76c4fd873bdaef09b0173e4b000cef3943c4f540
fernancumez <fernancumez@gmail.com>
2020-12-02 11:09:41 -0600

    adding interface types src/config/index.js

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
-export default {
+const Config: IConfig = {
   DATABASE: {
     URI: process.env.MONGODB_URI || "mongodb://localhost/photo-gallery",
     USER: process.env.USER,
@@ -7,3 +7,17 @@ export default {
   PORT: process.env.PORT || 4000,
   NODE_ENV: process.env.NODE_ENV || "development",
 };
+
+interface IConfig {
+  DATABASE: Idbconfig;
+  PORT: string | number;
+  NODE_ENV: string;
+}
+
+interface Idbconfig {
+  URI: string;
+  USER: string | undefined;
+  PASSWORD: string | undefined;
+}
+
+export default Config;
